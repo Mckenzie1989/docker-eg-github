44e4e476c0cfe87c07f941a5f295a3aa549d81cf
mohan <mohanseth07@gmail.com>
2021-05-31 19:23:51 +0530

    first commit

---
+++ src/main/java/com/dockereg/DockerEgGithubApplication.java
@@ -0,0 +1,13 @@
+package com.dockereg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEgGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerEgGithubApplication.class, args);
+	}
+
+}
